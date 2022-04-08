0c420a3dbdc40fb4d8077a5dd6ee7e6b7ba9bc0d
Sarah Mensah <mansahmensah18@gmail.com>
2022-04-07 19:30:43 -0700

    update with betty style

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 /**
- * main - entry point
- * Return: output with puts function
+ *main - entry point
+ *Return: output using printf
  */
 int main(void)
 {
 puts("\"Programming is like building a multilingual puzzle");
-return 0;
+return (0);
 }
