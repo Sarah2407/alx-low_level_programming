8575a52e2bd559ee4039f397c94e89299eedc337
Sarah Mensah <mansahmensah18@gmail.com>
2022-04-07 19:15:45 -0700

    updated with betty style

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 /**
- *main - entry point
- *Return: output using printf
+ * main - entry point
+ * Return: output using the printf function
  */
 int main(void)
 {
-printf("with proper grammar, but the outcome is a piece of art,");
-return 0;
+printf("with proper grammar, but the outcome is a piece of art,\n");
+return (0);
 }
