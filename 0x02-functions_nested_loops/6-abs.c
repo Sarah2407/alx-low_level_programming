0ff853360372f5dc7b395277d163471cf3ee7a26
Sarah <78199415+Sarah2407@users.noreply.github.com>
2022-04-12 17:25:34 -0700

    update print absolute of integer

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -1,7 +1,5 @@
 #include "main.h"
-#include <stdio.h>
 
-/**
 /**
  * _abs - return abs value
  * @n : number to check
@@ -11,19 +9,6 @@
 int _abs(int n)
 {
 
-if (n < 0)
-{
-return (n * (-1));
-}
-
-else if (n == 0)
-{
-return (0);
-}
-
-else
-{
-return (n);
-}
+return (n >= 0 ? n : n * -1);
 
 }
